8b3bf478bd92396679855114b5ba8b83c8c8d2b6
qLau <38216868+qLau@users.noreply.github.com>
2018-10-28 20:01:24 +0200

    Create vote.js

---
+++ commands/vote.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args, prefix) => {
+
+let voteembed = new Discord.RichEmbed()
+.setColor("RANDOM")
+.setTitle("Voteaza!")
+.setDescription(`**${args.join(" ")}**`)
+.setFooter("Funny.Minecraft-Romania.Ro | qLau ©");
+
+const msg = await message.channel.send({embed:voteembed})
+await msg.react("✅");
+await msg.react("❎");
+
+};
+module.exports.help = {
+    name: "vote"
+};
