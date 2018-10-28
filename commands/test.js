500aeae577a1ad1cf5d02735e5d7c00dd288a80b
qLau <38216868+qLau@users.noreply.github.com>
2018-10-28 19:40:50 +0200

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -1,1 +1,19 @@
+const Discord = require("discord.js");
 
+module.exports.run = (bot, message, args) => {
+
+let iq = [1, 23, 43 , 159, 200]
+let iqres = Math.floor(Math.random() * iq.length);
+
+let iqembed = new Discord.RichEmbed()
+.setTitle("Verifica-ti IQ-ul!")
+.setColor("RANDOM")
+.addField(`IQ-ul tau este`, iq[iqres])
+.setFooter("Funny.Minecraft-Romania.Ro | qLau");
+message.channel.send(iqembed);
+return
+
+}
+module.exports.help = {
+  name: "iq"
+}
