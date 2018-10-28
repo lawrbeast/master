dd8f305b79db4617885b5aa3f356893d081dc637
qLau <38216868+qLau@users.noreply.github.com>
2018-10-28 20:09:30 +0200

    Update vote.js

--- commands/vote.js
+++ commands/vote.js
@@ -2,6 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args, prefix) => {
 
+ if (!message.member.hasPermissions ('ADMINISTRATOR')) return
 let voteembed = new Discord.RichEmbed()
 .setColor("RANDOM")
 .setTitle("Voteaza!")
