b4f371c8ca5be106b006229dcb755deeea1171a3
qLau <38216868+qLau@users.noreply.github.com>
2018-10-28 19:58:26 +0200

    Update and rename test.js to help.js

--- commands/help.js
+++ commands/help.js
@@ -5,7 +5,7 @@ module.exports.run = (bot, message, args) => {
 let helpembed = new Discord.RichEmbed()
 .setColor("#990000")
 .setTitle("Funny.Minecraft-Romania.Ro")
-.setDescription("funny-serverinfo - iti arata informatiile despre server\nfunny-userinfo <@user> - iti arata informatiile unui user")
+.setDescription("funny-avatar - iti arata avatarul tau sau al unui membru\nfunny-serverinfo - iti arata informatiile despre server\nfunny-userinfo <@user> - iti arata informatiile unui user")
 .setFooter("Funny.Minecraft-Romania.Ro | qLau ©")
 message.channel.send({embed:helpembed})
 }
