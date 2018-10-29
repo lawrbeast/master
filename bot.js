35d94f709327cddf2ba97b5d351af596008b231f
qLau <38216868+qLau@users.noreply.github.com>
2018-10-29 17:51:08 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -39,14 +39,7 @@ bot.on("message", (message) => {
       let cmd = messageArray[0];
       let sender = message.author;
       let args = messageArray.slice(1);
-let msg = message.content.toUpperCase();
-	if(message.member.hasPermission("ADMINISTRATOR")) {
-		return;
-	}
-	if (msg.includes(`DISCORD.GG`)){
-		message.delete();
-		message.channel.send("**❌ Nu sunt permise linkurile care contin invite catre alt server!**").then(msg => {msg.delete(5000)})
-	} return;
+
       if(!message.content.startsWith(prefix)) return;
       let commandfile = bot.commands.get(cmd.slice(prefix.length));
       if(commandfile) commandfile.run(bot, message, args);
