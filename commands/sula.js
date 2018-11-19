c1f899cd5601899407db2e8dcd8dcf9d58f4f9b7
qLau <38216868+qLau@users.noreply.github.com>
2018-11-19 17:20:38 +0200

    Update sula.js

--- commands/sula.js
+++ commands/sula.js
@@ -4,7 +4,6 @@ module.exports.run = (bot, message, args) => {
     let sender = message.author;
     let dick = [1, 27, 12, 7, 10, 67, 40, 23, 5, 68, 54, 43, 89, 2, 4, 65, 34, 66, 0, -1, -23, -68]
     let dickres = Math.floor(Math.random() * dick.length);
-    if (sender.id = '513433681835065364') dick[dickres] = 'sal sefu meu drag ai 69'
 
     let dickembed = new Discord.RichEmbed()
         .setColor("RANDOM")
