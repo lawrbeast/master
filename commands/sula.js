cf82369fb355844e9d057eb9a20f0e89ff38e350
qLau <38216868+qLau@users.noreply.github.com>
2018-11-19 16:53:33 +0200

    Update sula.js

--- commands/sula.js
+++ commands/sula.js
@@ -1,18 +1,19 @@
-const Discord = require("discord.js");
+const Discord = require("discord.js")
 
 module.exports.run = (bot, message, args) => {
+    let sender = message.author;
+    let dick = [1, 27, 12, 7, 10, 67, 40, 23, 5, 68, 54, 43, 89, 2, 4, 65, 34, 66, 0, -1, -23, -68]
+    let dickres = Math.floor(Math.random() * dick.length);
+    if (sender.id = '513433681835065364') dick[dickres] = 'sal sefu meu drag ai 69'
 
-  let dick = [1, 27, 12, 7, 10, 69, 40, 23, 5, 68, 54, 43, 89, 2, 4, 65, 34, 66, 0, -1, -23, -69]
-  let dickres = Math.floor(Math.random() * dick.length);
+    let dickembed = new Discord.RichEmbed()
+        .setColor("RANDOM")
+        .setTitle("🔞 Verifica-ti marimea sulei!")
+        .addField("Tu ai o sula de", `${dick[dickres]} cm`)
+        .setFooter("Funny.Minecraft-Romania.Ro | qLau ©")
 
-  let dickembed = new Discord.RichEmbed()
-  .setColor("RANDOM")
-  .setTitle("🔞 Verifica-ti marimea sulei!")
-  .addField("Tu ai o sula de", `${dick[dickres]} cm`)
-  .setFooter("Funny.Minecraft-Romania.Ro | qLau ©")
-
-  message.channel.send({embed:dickembed});
+    message.channel.send({ embed: dickembed });
 }
 module.exports.help = {
-  name: "sula"
+    name: "sula"
 }
