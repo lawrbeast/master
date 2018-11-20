e1f274919fb599519f8ba8b630a3e7b64388e60b
qLau <38216868+qLau@users.noreply.github.com>
2018-11-20 19:23:29 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -3,9 +3,9 @@ const Discord = require("discord.js");
 module.exports.run = (bot, message, args) => {
 
 let helpembed = new Discord.RichEmbed()
-.setColor("#990000")
+.setColor("#cc0000")
 .setTitle("Funny.Minecraft-Romania.Ro")
-.setDescription("funny-sula - masoara-ti sula pe chat\nfunny-avatar - iti arata avatarul tau sau al unui membru\nfunny-serverinfo - iti arata informatiile despre server\nfunny-userinfo <@user> - iti arata informatiile unui user")
+.setDescription("funny-sula - masoara-ti sula pe chat\nfunny-avatar - iti arata avatarul tau sau al unui membru\nfunny-serverinfo - iti arata informatiile despre server\nfunny-userinfo <@user> - iti arata informatiile unui user\n\n**Music**\nfunny-play <link> pune o melodie (trebuie sa te conectezi pe voice channel)\nfunny-disconnect - opreste botul")
 .setFooter("Funny.Minecraft-Romania.Ro | qLau ©")
 message.channel.send({embed:helpembed})
 }
