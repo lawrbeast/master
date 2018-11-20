06bd46d82829cb9fbf102510410aebfd7ff2b049
qLau <38216868+qLau@users.noreply.github.com>
2018-11-20 19:29:47 +0200

    Update reload.js

--- commands/reload.js
+++ commands/reload.js
@@ -5,7 +5,7 @@ exports.run = (client, message, args) => {
     if (message.author.id !== "257491128671141888") return message.channel.send("⛔ **Acces interzis**");
 
     try {
-        delete require.cache[require.resolve(`./${args[0]}.js`)];
+        delete require.cache[require.resolve(`./commands/${args[0]}.js`)];
     } catch (e) {
 
         return message.channel.send(`Am intampinat o eroare la reload: ${args[0]}.js`);
