5e755e81e81d609d478e1d155838616a57822e8e
qLau <38216868+qLau@users.noreply.github.com>
2018-11-20 19:28:14 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -4,9 +4,8 @@ module.exports.run = (bot, message, args) => {
 
 let helpembed = new Discord.RichEmbed()
 .setColor("#cc0000")
-.setAuthor(message.bot)
 .setTitle("Funny.Minecraft-Romania.Ro")
-.setDescription("funny-sula - masoara-ti sula pe chat\nfunny-avatar - iti arata avatarul tau sau al unui membru\nfunny-serverinfo - iti arata informatiile despre server\nfunny-userinfo <@user> - iti arata informatiile unui user\n\n**Music**\nfunny-play <link> pune o melodie (trebuie sa te conectezi pe voice channel)\nfunny-disconnect - opreste botul")
+.setDescription("faunny-sula - masoara-ti sula pe chat\nfunny-avatar - iti arata avatarul tau sau al unui membru\nfunny-serverinfo - iti arata informatiile despre server\nfunny-userinfo <@user> - iti arata informatiile unui user\n\n**Music**\nfunny-play <link> pune o melodie (trebuie sa te conectezi pe voice channel)\nfunny-disconnect - opreste botul")
 .setFooter("Funny.Minecraft-Romania.Ro | qLau ©")
 message.channel.send({embed:helpembed})
 }
