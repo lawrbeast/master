346ec6cdebed967aa63ddf5dd52bdb4cd2cb203c
qLau <38216868+qLau@users.noreply.github.com>
2018-11-22 22:20:47 +0200

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -1,24 +1,25 @@
 const ytdl = require('ytdl-core');
-const ffmpeg = require("ffmpeg-binaries")
+const ffmpeg = require('ffmpeg-binaries');
+const fs = require ('fs');
+const search = require('yt-search');
 const Discord = require("discord.js");
 
-exports.run = async (client, message, args, ops) => {
-    
+exports.run = async (bot, message, args, ops) => {
+  let validate = ytdl.validateURL(args[0]);
+  if (!validate) {
+    let commandFile = require('./search.js')
+    return commandFile.run(bot, message, args);
+  }
+
     if (!message.member.voiceChannel) return message.channel.send('Te rog sa te conectezi la un canal vocal.');
-    if (!args[0]) return message.channel.send('Te rog sa introduci un link valid.');
+
     let connection = await message.member.voiceChannel.join();
-
-    let validate = await ytdl.validateURL(args[0]);
-    if (!validate) return message.channel.send('Sorry, please input a **valid** url following the command.');
-
     let info = await ytdl.getInfo(args[0]);
-    let audioFormats = ytdl.filterFormats(info.formats, 'audioonly');
-    let dispatcher = await connection.playStream(ytdl(args[0], {
-        filter: 'audioonly'
-    }));
-    message.channel.send(`🎵 Now playing: ${info.title}`);
-};
-
+    let dispatcher = await connection.playStream﻿(ytdl(args[0], {
+            filter: 'audioonly'
+        }));
+        message.channel.send(`🎵 Now playing: ${info.title}`);
+    };
 exports.help = {
   name:"play"
 }
