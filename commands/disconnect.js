3a9bfe660d196748d6f17871a8e3e08f735a2e5a
qLau <38216868+qLau@users.noreply.github.com>
2018-11-22 22:31:39 +0200

    Update disconnect.js

--- commands/disconnect.js
+++ commands/disconnect.js
@@ -1,5 +1,5 @@
 exports.run = (bot, message, args) => {
-
+    if (message.author.id !== "257491128671141888") return message.channel.send("Nu te pot lasa bosule, inca comenzile astea sunt in lucru.");
     if (!message.member.voiceChannel) return message.channel.send('Conecteaza-te la un voice channel!')
 
     if (!message.guild.me.voiceChannel) return message.channel.send('Nu sunt in niciun voice channel!')
