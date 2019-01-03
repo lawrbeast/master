307d7287ac6c62c339ab5e0b32c8fa38ff59bce2
qLau <38216868+qLau@users.noreply.github.com>
2019-01-03 18:47:42 +0200

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -5,21 +5,28 @@ const search = require('yt-search');
 const Discord = require("discord.js");
 
 exports.run = async (bot, message, args, ops) => {
-  if (message.author.id !== "257491128671141888") return message.channel.send("Nu te pot lasa bosule, inca comenzile astea sunt in lucru.");
   let validate = ytdl.validateURL(args[0]);
   if (!validate) {
     let commandFile = require('./search.js')
     return commandFile.run(bot, message, args);
   }
 
-    if (!message.member.voiceChannel) return message.channel.send('Te rog sa te conectezi la un canal vocal.');
+    if (!message.member.voiceChannel) return message.channel.send('Connect to a voice channel first.');
 
     let connection = await message.member.voiceChannel.join();
     let info = await ytdl.getInfo(args[0]);
-    let dispatcher = await connection.playStream﻿(ytdl(args[0], {
-            filter: 'audioonly'
-        }));
-        message.channel.send(`🎵 Now playing: ${info.title}`);
+    let dispatcher = await connection.playStream﻿(ytdl(args[0], { filter: 'audioonly'  }));
+    let pembed = new Discord.RichEmbed()
+    .setColor('#cc0000')
+    .setTitle('Now Playing')
+    .setDescription(`Song: ${info.title}`)
+    .setTimestamp(new Date)
+    .setAuthor(message.author.tag, message.author.avatarURL)
+        message.channel.send(pembed);
+
+        dispatcher.on('end', () => {
+            message.guild.me.voiceChannel.leave();
+        })
     };
 exports.help = {
   name:"play"
