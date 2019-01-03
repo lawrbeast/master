abbbed28d1f73b71d9e86bbe3c8d82c12a71733f
qLau <38216868+qLau@users.noreply.github.com>
2019-01-03 19:21:37 +0200

    Update and rename purge.js to clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -3,10 +3,10 @@ const Discord = require("discord.js");
 module.exports.run = (bot, message, args) => {
  //!clear 15
  let mspEmbed = new Discord.RichEmbed()
- .setDescription("❌ Nu poti face asta deoarece nu ai permisiunile necesare.")
+ .setDescription("❌ You don't have permission.")
  .setColor("#bc2731")
  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send({embed:mspEmbed});
- if(!args[0]) return message.channel.send("Te rog să introduci un numar de mesaje între 0 si 100!")
+ if(!args[0]) return message.channel.send("Please specify a number of messages that you want to delete.")
  message.channel.bulkDelete(args[0]).then(() => {
      message.channel.send.then(msg => msg.delete(10000));
  });
