a7606f29a1f37267f2beb54d2f7c5f136e7aea62
qLau <38216868+qLau@users.noreply.github.com>
2019-01-03 18:50:12 +0200

    Update search.js

--- commands/search.js
+++ commands/search.js
@@ -2,28 +2,27 @@ const Discord = require('discord.js');
 const search = require('yt-search');
 
 exports.run = (bot, message, args) => {
-  if (message.author.id !== "257491128671141888") return message.channel.send("Nu te pot lasa bosule, inca comenzile astea sunt in lucru.");
-  
+
   search(args.join(' '), function(err, res) {
-    if (err) return message.channel.send('Ceva a mers prost.')
+    if (err) return message.channel.send('Something went wrong.')
     let videos = res.videos.slice(0, 10);
     let resp = '';
     for (var i in videos) {
-      resp += `**[${parseInt(i)+1}]:** \`${videos[i].title}\`\n`;
+      resp += `**[${parseInt(i)+1}]:** \`${videos[i].title} - [${videos[i].timestamp}]\`\n`;
     }
 
     resp += `\n\`\`Alege un numar intre 1-${videos.length}\`\``;
     let sicon = message.author.avatarURL;
     const respembed = new Discord.RichEmbed()
-    .setColor('#cc0000')
-    .setTitle(`Rezultate pentru ${args.join(' ')}`)
-    .setThumbnail('https://fitness-aalsmeer.nl/wp-content/uploads/2016/09/icoon-play-Fitness-Aalsmeer-300x225.png')
+    .setColor('#34373d')
+    .setTitle(`Results for: ${args.join(' ')}`)
+    .setThumbnail('https://us.123rf.com/450wm/uasumy/uasumy1709/uasumy170900023/86140858-letter-m-logo-modern-monogram-paper-ribbon-material-design-style-identity-initial-emblem-mockup-over.jpg?ver=6')
     .setAuthor(message.author.username, sicon)
     .setDescription(resp)
     message.channel.send({embed:respembed});
 
     const filter = m => !isNaN(m.content) && m.content < videos.length+1 && m.content > - 0;
-    const collector = message.channel.createMessageCollector(filter);
+    const collector = message.channel.createMessageCollector(filter, {max: 1, time: 5000});
 
     collector.videos = videos;
     collector.once('collect', function(m) {
