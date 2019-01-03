e2c29bde5dd62cbe89c29be2af18da698fe10451
qLau <38216868+qLau@users.noreply.github.com>
2019-01-03 20:12:31 +0200

    Create ban.js

---
+++ commands/ban.js
@@ -0,0 +1,33 @@
+const Discord = require("discord.js");
+
+module.exports.run = (bot, message, args) => {
+  let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+  let bEmbed = new Discord.RichEmbed()
+  .setTitle('Command: $ban')
+  .setColor('#111111')
+  .setDescription('**Description:** Ban a user\n**Usage:** $ban [@user] [reason]\n**Example:** $ban [@qLau] reason-optional')
+  if(!bUser) return message.channel.send({embed:bEmbed})
+  if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("You don't have enough permission.");
+  if(bUser.hasPermission("ADMINISTRATOR")) return message.channel.send(":heavy_multiplication_x: You can't do that to administrators.");
+  let bReason = args.join(" ").slice(22);
+  if(!bReason) bReason = "no reason"
+
+  let banEmbed = new Discord.RichEmbed()
+  .setDescription(`Ban - Case | ${bUser}`)
+  .setColor("#111111")
+  .addField("User", bUser, true)
+  .addField("Moderator", message.author.username, true)
+  .addField("Reason", bReason, true)
+  .setTimestamp(new Date());
+
+  let banchannel = message.guild.channels.find(`name`, "logs");
+  if(!banchannel){
+  message.guild.member(bUser).ban(bReason);
+} else {
+  banchannel.send({embed:banEmbed});
+}
+}
+
+module.exports.help = {
+  name: "ban"
+}
