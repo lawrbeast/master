a767e06efe603243e10f7c6cca1c2c88c8c4185d
qLau <38216868+qLau@users.noreply.github.com>
2019-01-05 00:37:56 +0200

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -7,9 +7,7 @@ module.exports.run = (bot, message, args) => {
  .setColor("#bc2731")
  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send({embed:mspEmbed});
  if(!args[0]) return message.channel.send("Please specify a number of messages that you want to delete.")
- message.channel.bulkDelete(args[0]).then(() => {
-     message.channel.send.then(msg => msg.delete(10000));
- });
+ message.channel.bulkDelete(args[0])
 }
 module.exports.help = {
     name: "clear"
