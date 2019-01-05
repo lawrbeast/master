f86bfcd2d2ff5792eb4e9a50fecf845d55383b5b
qLau <38216868+qLau@users.noreply.github.com>
2019-01-05 20:17:34 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -7,7 +7,7 @@ let helpembed = new Discord.RichEmbed()
 .setAuthor('Master Commands', sicon)
 .addField('Fun Commands', '$emojify [text] - transform a text into emoji\n$meme - gives you a nice juicy meme\n$cat - gives you a cat image :cat:\n$dog- gives you a dog image :dog:\n$gif [gifname] - search a gif\n$avatar - gives your avatar\n')
 .addField('Moderation Commands', '$ban [@user] - ban a user\n$kick [@user] - kick a user\n$mute [@user] [time 1s,1m,1h,1d]- mute a user\n$serverinfo - gives you server information\n$roleinfo [@role] - gives you information about a specified role\n$userinfo - gives you information about a specified user\n$clear [0-100] - clear a number of messages\n')
-.addField('Music Commands', '$play [link or name] - play a song\n$disconnect - stop the bot')
+.addField('Music Commands', '$play [link or name] - play a song\n$disconnect - stop the bot\nMaster can\'t queue songs YET.')
 .addField('Game Trackers', '$fortnite [name] [platform] - gives you information about a specified Fortnite player\n$csgo [customURL] - gives you information about a specified CS GO player\n$osu [name] - gives you information about a specified OSU! player')
 message.channel.send({embed:helpembed})
 }
