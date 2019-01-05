0f1e75e2f5081ade76150bee4c4c85a59730d21e
qLau <38216868+qLau@users.noreply.github.com>
2019-01-05 23:08:58 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -24,6 +24,16 @@ fs.readdir("./commands", (err, files) => {
         bot.commands.set(props.help.name, props);
     });
 //CMD HANDLER
+const DBL = require('dblapi.js');
+const dbl = new DBL('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjUzMDQyNDQ4MTQ0NjEwMTAyMiIsImJvdCI6dHJ1ZSwiaWF0IjoxNTQ2NzE5NTcyfQ.LhqkfRCksuyJzDwCpKjVw_Rei011JhGjC0V8C7Yv-Mg', { webhookPort: process.env.PORT, webhookAuth: 'masterdbl9983' });
+dbl.webhook.on('ready', hook => {
+  console.log(`Webhook running at http://${hook.hostname}:${hook.port}${hook.path}`);
+});
+dbl.webhook.on('vote', vote => {
+  console.log(`User with ID ${vote.user} just voted!`);
+  let voteChannel = bot.channels.get("id");
+  voteChannel.send(`${vote.user} just voted the bot.`)
+});
 
 });
 //Bot
