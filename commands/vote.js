088e514eaece97ec70821027ef4cd03907633553
qLau <38216868+qLau@users.noreply.github.com>
2019-01-05 20:27:16 +0200

    Create vote.js

---
+++ commands/vote.js
@@ -0,0 +1,12 @@
+
+const Discord = require('discord.js');
+
+exports.run = (bot, message, args) => {
+
+  message.channel.send("Here is the vote link: https://discordbots.org/bot/530424481446101022")
+
+}
+
+exports.help = {
+  name: "vote"
+}
