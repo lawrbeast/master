992d76879a1eaaabe24651044430daa2b6ca9970
qLau <38216868+qLau@users.noreply.github.com>
2019-01-06 21:01:49 +0200

    Create invite.js

---
+++ commands/invite.js
@@ -0,0 +1,9 @@
+exports.run = (bot, message, args) => {
+
+   message.channel.send('Here is my invite link: https://discordapp.com/api/oauth2/authorize?client_id=530424481446101022&permissions=8&scope=bot')
+
+}
+
+exports.help = {
+    name: "invite"
+}
