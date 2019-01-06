da8622a5ba78fd3a024e785b1253ba5b8d2c7f94
qLau <38216868+qLau@users.noreply.github.com>
2019-01-06 03:09:12 +0200

    Update gif.js

--- commands/gif.js
+++ commands/gif.js
@@ -1,10 +1,11 @@
 const Discord = require("discord.js");
 const gifSearch = require('gif-search');
 
-exports.run = (client, message, args, tools) => {
+exports.run = (bot, message, args, tools) => {
+  if(!args[0]) return message.channel.send('Usage: $gif [gifname]')
   gifSearch.random(args[0]).then(
         gifUrl => {
-
+        
         const embed = new Discord.RichEmbed()
             .setColor(`#0a9678`)
             .setImage(gifUrl)
