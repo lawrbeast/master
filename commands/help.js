ea8246f67dd974033ba58f759c1616eeb9184d56
qLau <38216868+qLau@users.noreply.github.com>
2019-01-07 17:48:39 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -10,7 +10,7 @@ let helpembed = new Discord.RichEmbed()
 .addField('Music Commands', '$play [link or name] - play a song\n$disconnect - stop the bot\nMaster can\'t queue songs YET.')
 .addField('Game Trackers', '$fortnite [name] [platform] - gives you information about a specified Fortnite player\n$csgo [customURL] - gives you information about a specified CS GO player\n$osu [name] - gives you information about a specified OSU! player')
 .addField('Help Master with a vote!', '$vote - gives you the vote link (powered by discordbots.org)\n$invite - gives you the invite link')
-.setFooter('Support Server: https://discord.gg/zs3RUPw')
+.setFooter('Need help? Look here: masterbot.glitch.me')
 message.channel.send({embed:helpembed})
 }
 
